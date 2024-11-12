9a2afb327ef5136612de4240cd34f0831187f7b1
Abdellatif Labreche <abdellatif1898@gmail.com>
2024-11-12 10:45:24 +0100

    Improve paho mqtt example

--- examples/paho_mqtt.py
+++ examples/paho_mqtt.py
@@ -5,9 +5,16 @@ from mqtt_router import MQTTRouter
 router = MQTTRouter()
 
 
+# Subscribe to telemetry/devices/#/#/meta
 @router.add("telemetry/devices/<str:streaming_key>/<int:count>/meta")
 def handle_meta(message, context, streaming_key=None, count=None):
     print("Received meta '%s' with count '%d' from device '%s'" % (message, count, streaming_key))
+
+
+# Subscribe to telemetry/+
+@router.add("telemetry/<path:rest>")
+def handle_rest(message, context, rest=None):
+    print("Received telemetry message on path '%s'" % rest)
 
 
 def on_message(client, userdata, message):
